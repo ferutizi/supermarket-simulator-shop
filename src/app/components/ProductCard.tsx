196ed530a5222f684d49800ace4bfd84640f2c40
ferutizi <fer.utizi@gmail.com>
2024-04-02 19:59:06 -0300

    cards style

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useCart } from "../store/useCart"
 import { Product } from "../types"
 
 interface ProductCardProps {
@@ -5,39 +6,48 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const agregar = useCart((state) => state.addProduct)
   const { name, amount, img, placeAt, price, quantityPerBox, subTotal, supplier } = product
+
   return (
-    <article>
-      <div>
+    <article className="grid grid-cols-7 bg-cyan-950 p-4 gap-4 w-96">
+      <div className="col-span-5 bg-sky-950 relative">
         <h2>{name}</h2>
         <h3>{supplier}</h3>
-        <div>
+        <div className="flex">
           <img /* product */ />
-          <div>
-            <p>Lugar</p>
-            <p>{placeAt}</p>
-          </div>
-          <div>
-            <p>precio por unidad</p>
-            <p>{price}</p>
+          <div className="flex flex-col gap-4">
+            <div>
+              <p>Lugar</p>
+              <p>{placeAt}</p>
+            </div>
+            <div>
+              <p>precio por unidad</p>
+              <p>{price}</p>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="absolute bottom-0 right-0">
           <p>{quantityPerBox}</p>
           <img /* box */ />
         </div>
       </div>
 
-      <div>
-        <p>Cantidad</p>
+      <div className="col-span-2 flex flex-col bg-blue-950 justify-around items-center">
         <div>
-          <button></button>
-          <p>{amount}</p>
-          <button></button>
+          <p>Cantidad</p>
+          <div className="flex justify-between w-full items-center">
+            <button className="bg-white text-blue-950 rounded-full w-4 h-4 flex justify-center items-center">-</button>
+            <p>{amount}</p>
+            <button className="bg-white text-blue-950 rounded-full w-4 h-4 flex justify-center items-center">+</button>
+          </div>
+        </div>
+        <hr className="w-full border-white"></hr>
+        <div className="text-center">
           <p>Total</p>
           <p>{subTotal}</p>
-          <button>agregar</button>
         </div>
+        <button onClick={() => agregar(product)}>agregar</button>
       </div>
     </article>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import ProductCard from "./components/ProductCard";
+import { useCart } from "./store/useCart";
 import { useMainStore } from "./store/useMainStore";
 
 export default function Home() {
   const products = useMainStore((state) => state.products)
+  const cart = useCart((state) => state.products)
+  console.log(cart)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {products.map(product =>
-        <ProductCard product={product} />
-      )}
+      <section className="grid grid-cols-3 gap-8">
+        {products.map(product =>
+          <ProductCard product={product} />
+        )}
+      </section>
     </main>
   );
 }
