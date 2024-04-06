1f27faa02cfdeedb5b0eddfc3f524641509bc5bb
ferutizi <fer.utizi@gmail.com>
2024-04-06 14:21:34 -0300

    ProductsInCart component

---
+++ src/app/components/ProductInCart.tsx
@@ -0,0 +1,28 @@
+import { useCart } from "../store/useCart";
+import { Product } from "../types";
+
+interface ProductInCartProps {
+  product: Product
+}
+
+export default function ProductInCart({ product }: ProductInCartProps) {
+  const incrementAmount = useCart((state) => state.incrementAmount)
+  const decrementAmount = useCart((state) => state.decrementAmount)
+  const removeProduct = useCart((state) => state.removeProduct)
+
+  const { name, amount, price, subTotal } = product
+
+  return (
+    <li>
+      <p>{name}</p>
+      <div>
+        <button onClick={() => decrementAmount(product)}>-</button>
+        <p>{amount}</p>
+        <button onClick={() => incrementAmount(product)}>+</button>
+      </div>
+      <p>{price}</p>
+      <p>{subTotal}</p>
+      <button onClick={() => removeProduct(product)}>-</button>
+    </li>
+  )
+}
