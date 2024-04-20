a60712a9bc20983826003fbebd85c3b3d0acd2ee
ferutizi <fer.utizi@gmail.com>
2024-04-20 20:08:32 -0300

    fixed card component mobile design

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -15,7 +15,7 @@ export default function ProductCard({ product }: ProductCardProps) {
   return (
     <article className="flex flex-col w-[90vw] my-0 mx-auto xs:grid xs:grid-cols-7 bg-product-light p-4 gap-4 xs:w-[27rem] h-fit rounded-md font-extrabold">
       <div className="col-span-5 bg-product-dark relative rounded-md">
-        <h2 className="text-2xl whitespace-nowrap overflow-hidden text-ellipsis w-64 pl-4" title={product.name}>{name}</h2>
+        <h2 className="text-2xl whitespace-nowrap overflow-hidden text-ellipsis w-full pr-4 pl-4" title={product.name}>{name}</h2>
         <h3 className="text-slate-400 text-sm -translate-y-2 pl-4" title={product.supplier}>{supplier}</h3>
         <div className="flex items-center">
           <img
@@ -43,7 +43,7 @@ export default function ProductCard({ product }: ProductCardProps) {
         </div>
       </div>
 
-      <div className="col-span-2 flex flex-col bg-product-blue justify-around items-center rounded-md">
+      <div className="col-span-2 py-2 xs:py-0 flex xs:flex-col bg-product-blue justify-around items-center rounded-md">
         <div>
           <p>Cantidad</p>
           <div className="flex justify-between w-full items-center">
@@ -52,7 +52,7 @@ export default function ProductCard({ product }: ProductCardProps) {
             <button onClick={() => incrementAmount(product)} className="bg-white text-blue-950 rounded-full w-4 h-4 flex justify-center items-center">+</button>
           </div>
         </div>
-        <hr className="w-full border-white"></hr>
+        <hr className="hidden w-full border-white"></hr>
         <div className="text-center">
           <p className="text-sm">Total</p>
           <p className="text-sm">${subTotal.toFixed(2)}</p>
